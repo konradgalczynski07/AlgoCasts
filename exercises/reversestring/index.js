545f250e2e7a941c3ff032d61488d5cac8159fc0
konradgalczynski07 <konradgalczynski07@gmail.com>
2019-04-21 18:04:59 +0200

    Reverse String and Palindrome

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,35 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  let reversed = '';
+  let i = str.length - 1;
+  for (i; i > -1; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
 
 module.exports = reverse;
+
+function reverse1(str) {
+  const arr = str.split('');
+  arr.reverse();
+  return arr.join('');
+  // return str.split('').reverse().join('');
+}
+
+function reverse2(str) {
+  let reversed = '';
+  for (let character of str) {
+    reversed = character + reversed;
+  }
+  return reversed;
+}
+
+function reverse3(str) {
+  return str.split('').reduce((reversed, character) => {
+    return character + reversed;
+  }, '');
+  // return str.split('').reduce((rev, char) => char + rev, '');
+}
